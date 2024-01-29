05d011a0a507b143359dc367cdc2a62d2d2861cd
VittelOG <vitteloriginal@gmail.com>
2024-01-29 09:58:16 +0100

    updated background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
-<h1>this is the updated header</h1>
+<div id="mydiv">
+    <h1>Welcome to SvelteKit</h1>
+    <p>
+        Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+    </p>
+    <h1>this is the updated header</h1>
+</div>
+
+<style>
+    #mydiv {
+        background-color: red;
+        width: 100%;
+        height: 100%;
+    }
+</style>
